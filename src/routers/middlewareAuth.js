05250ff53fe3ea3b389bbadc3fa02aa2ded82dec
Larissa Arruda <127163325+LarissaArruda08@users.noreply.github.com>
2024-06-22 23:28:51 -0300

    refactor: adicionando rotas para validação token

--- src/routers/middlewareAuth.js
+++ src/routers/middlewareAuth.js
@@ -2,7 +2,7 @@ const jwtService = require('jsonwebtoken')
 
 module.exports = async (req, res, next) => {
     const routeUser = req.path
-    const nonSecurityRoutesUsers = ['/user/create', '/user/login', '/products', '/product/create', '/product/:id']
+    const nonSecurityRoutesUsers = ['/user/create', '/user/login', '/products', '/product/create']
     if(nonSecurityRoutesUsers.includes(routeUser)) {
         return next()
     }
